282bd3ddf7a23cb8de221e8466d2ca2b65d58b53
TLR8cheeseBro <theo.victoria9@gmail.com>
2024-10-10 19:53:00 -0400

    B: got the word count working - used len()

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+#working variables for full scope
+print("Welcome to Xerath's word counting compendium")
+print("counting words in the tome . . .")
+def word_count(text):
+    words = text.split()
+    return len(words)
+
+
 def main():
     with open("BookBot/books/frankenstein.txt") as f:
-        # . . .
-        print("im picking a book")
         book_contents = f.read()
-
+    
+    word_count_result = word_count(book_contents)
+    print(f"I've counted {word_count_result} words")
 main()
-
-print("im done reading")
