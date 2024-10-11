12e5871ce42ba8ae520862ef8288ebc383fbb0a0
TLR8cheeseBro <theo.victoria9@gmail.com>
2024-10-10 22:49:14 -0400

    Ca: finish the dictionary count Dont forget that dictionaries automatically update values so just iterate over the range with increments

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-#working variables for full scope
-from more_itertools import split_into
-
+#lets yeet this wheat
 
 print("Welcome to Xerath's word counting compendium")
 print("counting words in the tome . . .")
@@ -14,6 +12,15 @@ def letter_count(text):
         count_letters += 1
     return count_letters
 
+def per_letter_count(text):
+    lower_case = text.lower()
+    per_letter = {}
+    for letter in lower_case:
+        if letter in per_letter:
+            per_letter[letter] += 1
+        else: 
+            per_letter[letter] = 1
+    return per_letter
 
 def main():
     with open("BookBot/books/frankenstein.txt") as f:
@@ -21,34 +28,12 @@ def main():
     
     word_count_result = word_count(book_contents)
     letter_count_results = letter_count(book_contents)
+    per_letter_count_results = per_letter_count(book_contents)
     print(f"I've Mystically counted {word_count_result} words")
     print(f"I've Majestically counted {letter_count_results} letters")
-    "Now lets get fancy, for each character
-             There are 0 A's
-             There are 0 B's
-             There are 0 C's
-             There are 0 D's
-             There are 0 E's
-             There are 0 F's
-             There are 0 G's
-             There are 0 H's
-             There are 0 I's
-             There are 0 J's
-             There are 0 K's
-             There are 0 L's
-             There are 0 M's
-             There are 0 N's
-             There are 0 O's
-             There are 0 P's
-             There are 0 Q's
-             There are 0 R's
-             There are 0 S's
-             There are 0 T's
-             There are 0 U's
-             There are 0 V's
-             There are 0 W's
-             There are 0 X's
-             There are 0 Y's
-             There are 0 Z's
-            all Magnanimously counted"
-main()
+    print("Now lets get fancy")
+    print(per_letter_count_results)
+    print("Are you impressed with me?")
+
+if __name__ == "__main__":
+    main()
