acd0bc8cd0ecf8b3548cd7d030fe3383718602b4
TLR8cheeseBro <theo.victoria9@gmail.com>
2024-10-16 20:14:09 -0400

    I did it! it was accurate too!

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #lets yeet this wheat
 
-print("Welcome to Xerath's word counting compendium")
+print("Welcome to Xerath's Word Counting Compendium")
 print("counting words in the tome . . .")
 def word_count(text):
     words = text.split()
@@ -12,8 +12,8 @@ def letter_count(text):
         count_letters += 1
     return count_letters
 
-def sort_on(dict, key_letter):
-    return dict[key_letter]
+def sort_on(dict):
+    return dict['count']
 
 def only_alpha(text):
     only_alpha_text = ""
@@ -35,7 +35,7 @@ def per_letter_count(text):
 
     give_me_alpha = only_alpha(lower_case)
 
-    #part 2 make text only lowercase, will count occurrences
+    #part 2 make alpha_text only lowercase, will count occurrences
     
     for letter in give_me_alpha:
         if letter in per_letter:
@@ -47,8 +47,8 @@ def per_letter_count(text):
     for char, count in per_letter.items():
         list_of_per_letter.append({'char': char, 'count': count})
     
-    print(list_of_per_letter)
-    return per_letter , list_of_per_letter
+    list_of_per_letter.sort(reverse=True, key=sort_on)
+    return list_of_per_letter
 
 
 def main():
@@ -63,9 +63,20 @@ def main():
     print(f"I've Majestically counted {letter_count_results} letters")
     print("Now lets get fancy")
     print(per_letter_count_results)
-    print("Are you impressed with me? Good cuz im not done yet!")
-    print("Here is a pretty report")
-    
+    print("Are you impressed with me?")
+    print("Let me make it pretty for you")
+    new_line_counter = 0
+    for line in per_letter_count_results:
+        if new_line_counter <= 26:
+            print(f"Per letter report{per_letter_count_results[new_line_counter]}")
+            new_line_counter += 1
+    print("This should be easier to read")
+    print(".")
+    print(".")
+    print(".")
+    print(".")
+    print(".")
+    print("Thank you for using Xerath's Word Counting Compendium")
 
 if __name__ == "__main__":
     main()
